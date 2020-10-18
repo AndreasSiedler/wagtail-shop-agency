d3ea2ea4f583fb08f6333c6ddf130d3d925254e3
Andreas Siedler <andreas.siedler@gmail.com>
2020-10-18 15:21:16 +0000

    Added subpage model

--- app/home/models.py
+++ app/home/models.py
@@ -9,4 +9,13 @@ class HomePage(MetadataPageMixin, FlexPage):
     HomePage model which inherits from Flexpage
     Hint: Here the home template is used which extends the flex_page template
     """
+    subpage_types = ['blog.BlogIndexPage', 'form.FormPage', 'home.SubPage']
+
+
+class SubPage(MetadataPageMixin, FlexPage):
+    """
+    SubPage model which inherits from Flexpage
+    Hint: Here the subpage template is used which extends the flex_page template
+    """
     subpage_types = ['blog.BlogIndexPage', 'form.FormPage']
+    parent_page_types = ['home.HomePage']
